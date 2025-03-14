eacf37dbbf2d14ae9ff7ffd6cb7587470da8441f
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2025-03-14 23:47:43 +0530

    Changes to data

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,18 +1,29 @@
 <script>
-    import Hero from "../components/Hero.svelte";
-    let { data } = $props();
+  import Hero from "../components/Hero.svelte";
+  import UqHero from "../components/UQ-Hero.svelte";
+  let { data } = $props();
 </script>
 
 <svelte:head>
-    <title>Un-quizzes</title>
+  <title>Un-quizzes</title>
 </svelte:head>
 {#each data.body.people as person}
-    <div class="w-full flex flex-col items-center justify-center">
-        <Hero
-            name={person.name}
-            brief={person.brief}
-            imageCaption={person.imageCaption}
-            imageURL={person.imageURL}
-        />
-    </div>
+  <div class="w-full flex flex-col items-center justify-center">
+    <Hero
+      name={person.name}
+      brief={person.brief}
+      imageCaption={person.imageCaption}
+      imageURL={person.imageURL}
+    />
+  </div>
+{/each}
+{#each data.body.people as person}
+  <div class="w-full flex flex-col items-center justify-center">
+    <UqHero
+      name={person.name}
+      brief={person.brief}
+      imageCaption={person.imageCaption}
+      imageURL={person.imageURL}
+    />
+  </div>
 {/each}
