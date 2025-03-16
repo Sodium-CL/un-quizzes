0abb5373e7bb93b0ce8c2ee56ecd11e44d681b6a
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2025-03-16 13:39:46 +0530

    re configured project

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,30 +1,30 @@
 <script>
-  import Hero from "../components/Hero.svelte";
-  import UqHero from "../components/UQ-Hero.svelte";
-  let { data } = $props();
-  const unquizzesId = "unquizzes-hero";
+    import Hero from "../components/Hero.svelte";
+    import UqHero from "../components/UQ-Hero.svelte";
+    let { data } = $props();
+    const unquizzesId = "unquizzes-hero";
 </script>
 
 <svelte:head>
-  <title>Un-quizzes</title>
+    <title>Un-quizzes</title>
 </svelte:head>
 {#each data.body.people as person}
-  <div class="w-full flex flex-col items-center justify-center">
-    <Hero
-      name={person.name}
-      brief={person.brief}
-      imageCaption={person.imageCaption}
-      imageURL={person.imageURL}
-      {unquizzesId}
-    />
-  </div>
+    <div class="w-full flex flex-col items-center justify-center">
+        <Hero
+            name={person.name}
+            brief={person.brief}
+            imageCaption={person.imageCaption}
+            imageURL={person.imageURL}
+            {unquizzesId}
+        />
+    </div>
 {/each}
 {#each data.body.uqHero as uqData}
-  <div id={unquizzesId} class="w-full">
-    <UqHero
-      title={uqData.title}
-      description={uqData.description}
-      imageURLs={uqData.imageURLs}
-    />
-  </div>
+    <div id={unquizzesId} class="w-full">
+        <UqHero
+            title={uqData.title}
+            description={uqData.description}
+            imageURLs={uqData.imageURLs}
+        />
+    </div>
 {/each}
