d1986bd8eeb16ad59e6f7e6dfca48b238156ed54
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2025-03-23 22:08:26 +0530

    Changes to Coudflare pages

---
+++ src/components/un-q-hero.svelte
@@ -0,0 +1,51 @@
+<script>
+    let props = $props();
+</script>
+
+<div
+    class="h-[100vh] bg-uq-purple grid grid-rows-5 grid-columns-5 justify-items-center items-center"
+>
+    <div
+        class="col-span-5 font-semibold text-uq-white uppercase tracking-widest text-xl/8"
+    >
+        "Un-Quizzes"?
+    </div>
+    <div class="row-span-2 row-start-2 m-20 p-20">
+        <enhanced:img
+            class="rounded-xl w-auto h-auto"
+            src="../lib/assets/images/un-q-hero/1.png"
+            alt="Event Logo"
+        />
+    </div>
+    <div class="column-span-3 row-span-4 row-start-2 text-uq-white">
+        <p>
+            Usually, competitive quizzing tests one’s general knowledge inside a
+            relatively formal structure. The focus tends to be on winning as
+            much as learning (if not more). Un-quizzes are a creative departure
+            from this approach, placing curiosity front and centre while the
+            competitive elements take a comfy backseat, setting the stage for
+            involved participation sans the stress, and meaningful engagement.
+        </p>
+    </div>
+    <div class="row-span-2 row-start-2 column-start-5 m-20 p-20">
+        <enhanced:img
+            class="rounded-xl w-auto h-auto"
+            src="../lib/assets/images/un-q-hero/2.png"
+            alt="Event Logo"
+        />
+    </div>
+    <div class="row-span-2 row-start-4 m-20 p-20">
+        <enhanced:img
+            class="rounded-xl w-auto h-auto"
+            src="../lib/assets/images/un-q-hero/3.png"
+            alt="Event Logo"
+        />
+    </div>
+    <div class="row-span-2 row-start-4 column-start-5 m-20 p-20">
+        <enhanced:img
+            class="rounded-xl w-auto h-auto"
+            src="../lib/assets/images/un-q-hero/4.png"
+            alt="Event Logo"
+        />
+    </div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
-import adapter from "svelte-adapter-azure-swa";
-
+import adapter from "@sveltejs/adapter-cloudflare";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // See below for an explanation of these options
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
+      platformProxy: {
+        configPath: undefined,
+        environment: undefined,
+        persist: undefined,
+      },
+    }),
   },
 };
 
