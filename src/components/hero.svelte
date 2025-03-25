b927810a3493f6d0e3bbbf50b2320b61c1ee1c8a
bitsXbytes <12557052+bitsnorbytes@users.noreply.github.com>
2025-03-25 20:42:37 +0530

    Responsive design

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -4,33 +4,41 @@
     import HeroImage from "$assets/images/hero/1.png?enhanced";
 </script>
 
-<div class="h-[100vh] bg-uq-purple mx-auto flex items-center justify-center">
-    <div class="flex items-center justify-end px-12">
-        <div class="mt-6 text-xl/8 text-gray-700">
+<div
+    class="h-[100vh] bg-uq-purple mx-auto flex flex-col md:flex-row items-center justify-center"
+>
+    <div
+        class="flex flex-col md:flex-row items-center justify-center md:justify-end px-12"
+    >
+        <div
+            class="flex flex-col items-center md:items-start md:mt-6 text-xl/8 text-gray-700"
+        >
             <span class="font-semibold text-uq-white uppercase tracking-widest"
                 >Yooti Bhansali</span
             ><br /><br />
-            <span class="font-abhaya-libre text-uq-white text-7xl">
+            <span class="font-abhaya-libre text-uq-white text-3xl md:text-7xl">
                 designer & facilitator of curiosity driven "un-quizzes"</span
             >
         </div>
     </div>
-    <div class="flex items-center justify-between p-12">
-        <figure class="w-1/2">
+    <div
+        class="flex flex-col md:flex-row items-center justify-between p-6 md:p-12"
+    >
+        <figure class="w-7/12 md:w-1/2">
             <enhanced:img
                 class="rounded-3xl w-full h-auto"
                 src={HeroImage}
                 alt="Event Logo"
             />
             <figcaption
-                class="text-sm font-light italic text-uq-white w-full m-2"
+                class="text-sm font-light italic text-uq-white w-full md:m-2"
             >
                 Pictured here: conducting Bing-oh, which incorporates the game
                 of Tambola as the competitive element
             </figcaption>
         </figure>
-        <a href="#{props.unquizzesId}" class="mx-8">
-            <Next width="10em" height="10em" fill="#f4e7d9" />
+        <a href="#{props.unquizzesId}" class="w-15 md:w-30 h-15 md:h-30 mx-8">
+            <Next fill="#f4e7d9" />
         </a>
     </div>
 </div>
